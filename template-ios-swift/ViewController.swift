ada9bb09e71849c027151766486b4b62ffdedd5f
Mario Galvão <mcasarigalvao@gmail.com>
2020-09-16 10:11:49 -0300

    Initial Commit

---
+++ template-ios-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  template-ios-swift
+//
+//  Created by Mário Galvao on 16/09/20.
+//  Copyright © 2020 Open Mind House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
